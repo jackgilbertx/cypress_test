669b332420ce7f1e69f08c3970f343d103d5bd54
JackGilbert <jack_gilbert@crgl-thirdparty.com>
2020-04-20 12:38:32 -0500

    commit with drone.yml

--- xxx.sh
+++ xxx.sh
@@ -1,15 +1,2 @@
-#!/bin/bash
-
-
-json=$(echo ',
-  "video": true,
-  "reporter": "mochawesome",
-  "reporterOptions": {
-    "reportDir": "cypress/report/mochawesome-report",
-    "overwrite": false,
-    "html": false,
-    "json": true
-  },
-  "screenshotsFolder": "mochawesome-report/assets"')
-
-  sed -i '$i'"$(echo ${json})" cypress.json
+secret 
+57e8512ce3af3495738a86c99b23dee6
